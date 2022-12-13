7386fb2ed0d5e0b1f551e6f25a8be9f137c9649b
Gaëtan <61387206+GaetanOff@users.noreply.github.com>
2022-12-13 09:36:43 +0100

    Updated reservation

--- app/infoChambre.php
+++ app/infoChambre.php
@@ -17,64 +17,49 @@ if (!isRoomExists(id)) {
 define('room', getRoom(id));
 ?>
 
-<section>
-    <div class="container mx-auto px-4">
-        <div class="flex flex-col justify-center items-center mb-6">
-            <div>
-                <h1 class="font-bold"><?php echo room['name'] ?></h1>
-            </div>
-            <div>
-                <img src="<?php echo room['firstImage'] ?>">
-            </div>
-            <div>
-                <div class="flex flex-col justify-center items-center py-6 md:py-0">
-                    <div class="flex items-center">
-                        <svg class="w-5 h-5 text-yellow-500" fill="currentColor" viewBox="0 0 20 20"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg class="w-5 h-5 text-yellow-500" fill="currentColor" viewBox="0 0 20 20"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg class="w-5 h-5 text-yellow-500" fill="currentColor" viewBox="0 0 20 20"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg class="w-5 h-5 text-yellow-500" fill="currentColor" viewBox="0 0 20 20"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
-                        <svg class="w-5 h-5 text-yellow-500" fill="currentColor" viewBox="0 0 20 20"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
+    <section>
+        <div class="pt-20">
+            <div class="container mx-auto px-4">
 
-                        <div class="flex items-center">
-                            <div>
-                                <span class="ml-3"><?php echo room['stars'] ?> étoiles</span>
-                            </div>
-                            <div class="border-l-2 border-black ml-3">
-                                <span class="text-sm ml-3"><?php echo room['rating'] ?> avis</span>
-                            </div>
-                        </div>
+                <div class="mb-16 max-w-2xl mx-auto text-center">
+                    <div class=" text-center mb-16">
+                        <h1 class="mb-2 text-4xl lg:text-5xl font-bold font-heading">
+                            <?php echo room['name'] ?>
+                        </h1>
                     </div>
                 </div>
-            </div>
-            <div>
-                <span class="text-sm font-semibold"></span>&nbsp;<span
-                        class="font-bold text-xl"><?php echo room['price'] ?></span>&nbsp;<span
-                        class="text-sm font-semibold">€</span>
-            </div>
-            <div>
-                <button type="button"
-                        class="transform hover:scale-105 motion-reduce:transform-none duration-300 text-white bg-black text-md text-gold-custom border duration-200 border-white font-medium text-sm px-3 py-1.5 text-center mr-3 md:mr-0">
-                    Reserver cette chambre
-                </button>
+
+                <div class="max-w-4xl flex items-center flex-wrap mx-auto lg:my-0">
+
+                    <div
+                            class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none mx-6 lg:mx-0"
+                            id="profile">
+                        <div class="p-4 md:p-12 text-center lg:text-left">
+                            <div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center"
+                                 style="background-image: url(<?php echo room['firstImage'] ?>)"></div>
+                            <div class="text-3xl font-body pt-8 lg:pt-0 mb-4">
+                                <span class="text-sm font-semibold"></span>&nbsp;<span
+                                        class="font-bold text-xl"><?php echo room['price'] ?></span>&nbsp;<span
+                                        class="text-sm font-semibold">€</span>
+                            </div>
+                            <button type="button"
+                                    class="transform hover:scale-105 motion-reduce:transform-none duration-300 text-white bg-black text-md text-gold-custom border duration-200 border-white font-medium text-sm px-3 py-1.5 text-center mr-3 md:mr-0">
+                                Reserver cette chambre
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="w-full lg:w-2/5">
+                        <div class="relative" style="z-index: 0;">
+                            <img alt="Photo de profil" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block w-full h-full"
+                                 src="<?php echo room['firstImage'] ?>">
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
 
 <?php include('./include/footer.php'); ?>
