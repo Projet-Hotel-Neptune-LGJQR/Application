89285ddd01ee1070dbe1f715996cb8d0573e9296
Gaëtan <61387206+GaetanOff@users.noreply.github.com>
2022-12-13 09:41:13 +0100

    Updated rooms

--- app/admin/room/add.php
+++ app/admin/room/add.php
@@ -35,78 +35,78 @@ if (isset($_POST['room-name']) && isset($_POST['room-star'])
 
 ?>
 
-<section>
-    <div class="pt-6">
-        <div class="container mx-auto px-4">
-            <div class="container mx-auto p-4 bg-white">
-                <div class="w-full md:w-1/2 lg:w-1/3 mx-auto my-12">
-                    <h1 class="text-lg font-bold">Ajouter une chambre</h1>
-                    <form class="flex flex-col mt-4" enctype="multipart/form-data" action="/admin/room/add.php"
-                          method="post" data-turbo="false">
-                        <label>
-                            <input
-                                    type="text"
-                                    name="room-name"
-                                    required
-                                    class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-                                    placeholder="Nom de la chambre"
-                            />
-                        </label>
-                        <label>
-                            <input
-                                    type="number"
-                                    name="room-star"
-                                    required
-                                    class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-                                    placeholder="Note de la chambre"
-                            />
-                        </label>
-                        <label>
-                            <input
-                                    type="number"
-                                    name="room-rating"
-                                    required
-                                    class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-                                    placeholder="Nombre de note de la chambre"
-                            />
-                        </label>
-                        <label>
-                            <input
-                                    type="number"
-                                    name="room-price"
-                                    required
-                                    class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-                                    placeholder="Prix de la chambre"
-                            />
-                        </label>
-                        <label>
-                            <input
-                                    type="file"
-                                    name="room-firstimage"
-                                    required
-                                    accept="image/png, image/jpeg"
-                                    class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
-                                    placeholder="Première image de la chambre"
-                            />
-                        </label>
-                        <button
-                                type="submit"
-                                class="mt-4 px-4 py-3 leading-6 text-base rounded-md border border-transparent text-white bg-black hover:bg-white hover:text-black border duration-200 border-black text-gold-custom cursor-pointer inline-flex items-center w-full justify-center items-center font-medium"
-                        >
-                            Ajouter la chambre
-                        </button>
-                        <?php if (count($errors) > 0) : ?>
-                            <div class="error">
-                                <?php foreach ($errors as $error) : ?>
-                                    <p><?php echo $error ?></p>
-                                <?php endforeach ?>
-                            </div>
-                        <?php endif ?>
-                    </form>
+    <section>
+        <div class="pt-6">
+            <div class="container mx-auto px-4">
+                <div class="container mx-auto p-4 bg-white">
+                    <div class="w-full md:w-1/2 lg:w-1/3 mx-auto my-12">
+                        <h1 class="text-lg font-bold">Ajouter une chambre</h1>
+                        <form class="flex flex-col mt-4" enctype="multipart/form-data" action="/admin/room/add.php"
+                              method="post" data-turbo="false">
+                            <label>
+                                <input
+                                        type="text"
+                                        name="room-name"
+                                        required
+                                        class="px-4 py-3 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                                        placeholder="Nom de la chambre"
+                                />
+                            </label>
+                            <label>
+                                <input
+                                        type="number"
+                                        name="room-star"
+                                        required
+                                        class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                                        placeholder="Note de la chambre"
+                                />
+                            </label>
+                            <label>
+                                <input
+                                        type="number"
+                                        name="room-rating"
+                                        required
+                                        class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                                        placeholder="Nombre de note de la chambre"
+                                />
+                            </label>
+                            <label>
+                                <input
+                                        type="number"
+                                        name="room-price"
+                                        required
+                                        class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                                        placeholder="Prix de la chambre"
+                                />
+                            </label>
+                            <label>
+                                <input
+                                        type="file"
+                                        name="room-firstimage"
+                                        required
+                                        accept="image/png, image/jpeg"
+                                        class="px-4 py-3 mt-4 w-full rounded-md bg-gray-100 border-transparent focus:border-gray-500 focus:bg-white focus:ring-0 text-sm"
+                                        placeholder="Première image de la chambre"
+                                />
+                            </label>
+                            <button
+                                    type="submit"
+                                    class="mt-4 px-4 py-3 leading-6 text-base rounded-md border border-transparent text-white bg-black hover:bg-white hover:text-black border duration-200 border-black text-gold-custom cursor-pointer inline-flex items-center w-full justify-center items-center font-medium"
+                            >
+                                Ajouter la chambre
+                            </button>
+                            <?php if (count($errors) > 0) : ?>
+                                <div class="error">
+                                    <?php foreach ($errors as $error) : ?>
+                                        <p><?php echo $error ?></p>
+                                    <?php endforeach ?>
+                                </div>
+                            <?php endif ?>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
 <?php include('../../include/footer.php'); ?>
--- app/admin/users.php
+++ app/admin/users.php
@@ -10,7 +10,8 @@ include "../database/database.php";
 
 define('users', getAccs());
 
-function delUser($email) {
+function delUser($email)
+{
     deleteAcc($email);
 
     echo "<meta http-equiv=\"refresh\" content=\"0;URL=/admin/users.php\">";
@@ -21,34 +22,34 @@ if (isset($_GET['user'])) {
 }
 ?>
 
-<section>
-    <div class="pt-6">
-        <div class="container mx-auto px-4">
-            <div class="grid grid-cols-1">
-                <?php foreach (users as $user) : ?>
-                    <div class="bg-gray-300 rounded-md sm:h-80 md:h-80 lg:h-32 flex flex-col lg:flex-row justify-between mb-3">
-                        <div class="flex flex-col justify-center items-center px-4 py-6 md:py-0">
-                            <h1>Prénom:</h1>
-                            <span class="font-bold"><?php echo $user[1] ?></span>
-                        </div>
-                        <div class="flex flex-col justify-center items-center px-4 py-6 md:py-0">
-                            <h1>E-Mail:</h1>
-                            <span class="font-bold"><?php echo $user[2] ?></span>
-                        </div>
-                        <div class="pb-6 md:pb-0 lg:border-l-2 lg:border-black flex flex-col justify-center items-center lg:space-y-6 mr-12">
-                            <div>
-                                <a type="button"
-                                   href="/admin/users.php?user=<?php echo $user[2] ?>"
-                                        class=" ml-12 transform hover:scale-105 motion-reduce:transform-none duration-300 bg-black text-md text-gold-custom duration-200 font-medium text-sm px-3 py-1.5 text-center mr-3 md:mr-0">
-                                    Supprimer
-                                </a>
+    <section>
+        <div class="pt-6">
+            <div class="container mx-auto px-4">
+                <div class="grid grid-cols-1">
+                    <?php foreach (users as $user) : ?>
+                        <div class="bg-gray-300 rounded-md sm:h-80 md:h-80 lg:h-32 flex flex-col lg:flex-row justify-between mb-3">
+                            <div class="flex flex-col justify-center items-center px-4 py-6 md:py-0">
+                                <h1>Prénom:</h1>
+                                <span class="font-bold"><?php echo $user[1] ?></span>
+                            </div>
+                            <div class="flex flex-col justify-center items-center px-4 py-6 md:py-0">
+                                <h1>E-Mail:</h1>
+                                <span class="font-bold"><?php echo $user[2] ?></span>
+                            </div>
+                            <div class="pb-6 md:pb-0 lg:border-l-2 lg:border-black flex flex-col justify-center items-center lg:space-y-6 mr-12">
+                                <div>
+                                    <a type="button"
+                                       href="/admin/users.php?user=<?php echo $user[2] ?>"
+                                       class=" ml-12 transform hover:scale-105 motion-reduce:transform-none duration-300 bg-black text-md text-gold-custom duration-200 font-medium text-sm px-3 py-1.5 text-center mr-3 md:mr-0">
+                                        Supprimer
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                <?php endforeach ?>
+                    <?php endforeach ?>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
 <?php include('../include/footer.php'); ?>
